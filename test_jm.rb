5dbb0516509ae1f1d5b77b25f8fcbe7a19ab782b
Jason Markson <jasonmarkson76@gmail.com>
2020-09-25 12:21:48 +0000

    Automatically backed up by Learn

--- test_jm.rb
+++ test_jm.rb
@@ -1,1 +1,4 @@
 require_relative 'lib/get_requester.rb'
+
+get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.parse_json
